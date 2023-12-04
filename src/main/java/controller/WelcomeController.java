17de250f689d37d618205f4b83a0bee29c8c8351
tomcscscs <bright.chan88@gmail.com>
2023-12-04 10:00:53 +0900

    안녕하세요 두번째주석입니다.

--- src/main/java/controller/WelcomeController.java
+++ src/main/java/controller/WelcomeController.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class WelcomeController extends HttpServlet{
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {//dd
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {//이건 새로운 주석입니다.
 
 		req.getRequestDispatcher("/WEB-INF/view/welcome.jsp").forward(req, resp);
 	}
